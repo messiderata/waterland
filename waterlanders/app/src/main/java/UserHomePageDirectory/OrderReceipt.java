f715b4620831ed341adf41951393bc79d83db605
ChugxScript <arw.oloroso@gmail.com>
2024-08-31 14:42:42 +0800

    [update] saving delivery_address and additional_message to firebase in Cash payment

--- waterlanders/app/src/main/java/UserHomePageDirectory/OrderReceipt.java
+++ waterlanders/app/src/main/java/UserHomePageDirectory/OrderReceipt.java
@@ -16,6 +16,9 @@ import java.util.Map;
 
 public class OrderReceipt extends AppCompatActivity {
     FirebaseFirestore db;
+    private AddedItems addedItems;
+    private Map<String, Object> currentDefaultAddress;
+    private String modeOfPayment, additionalMessage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,11 +30,13 @@ public class OrderReceipt extends AppCompatActivity {
 
         // Retrieve intent data
         Intent intent = getIntent();
-        AddedItems addedItems = (AddedItems) intent.getSerializableExtra("addedItems");
-        String userAddress = intent.getStringExtra("userAddress");
+        addedItems = (AddedItems) intent.getSerializableExtra("addedItems");
+        currentDefaultAddress = (Map<String, Object>) intent.getSerializableExtra("deliveryAddress");
+        modeOfPayment = (String) intent.getSerializableExtra("modeOfPayment");
+        additionalMessage = (String) intent.getSerializableExtra("additionalMessage");
 
         // Save order to Firebase
-        saveOrderToFirebase(addedItems, userAddress);
+        generateUniqueDocumentId();
 
         // Set up button and its click listener
         button_btn.setOnClickListener(view -> {
@@ -41,16 +46,19 @@ public class OrderReceipt extends AppCompatActivity {
         });
     }
 
-    private void saveOrderToFirebase(AddedItems addedItems, String userAddress) {
-        generateUniqueDocumentId(addedItems, userAddress);
+    // flow
+    // this first generate a random id from 'pendingOrders' collection
+    // then it will check if that id is already existed in 'onDelivery' collection
+    // this way we can pass the the id from a collection to another without conflict
+    // this way as well we can track the order properly without being confused because of changing the id
+    // then if the ids are existed then generate again
+    // else save the order details to the firebase firestore
+    private void generateUniqueDocumentId() {
+        String documentId = db.collection("pendingOrders").document().getId();
+        checkDocumentInOnDelivery(documentId);
     }
 
-    private void generateUniqueDocumentId(AddedItems addedItems, String userAddress) {
-        String documentId = db.collection("pendingOrders").document().getId(); // Generate a new document ID
-        checkDocumentInOnDelivery(documentId, addedItems, userAddress);
-    }
-
-    private void checkDocumentInOnDelivery(String documentId, AddedItems addedItems, String userAddress) {
+    private void checkDocumentInOnDelivery(String documentId) {
         db.collection("onDelivery")
                 .document(documentId)
                 .get()
@@ -58,10 +66,10 @@ public class OrderReceipt extends AppCompatActivity {
                     if (task.isSuccessful() && task.getResult() != null) {
                         if (task.getResult().exists()) {
                             // Document ID already exists, generate a new one
-                            generateUniqueDocumentId(addedItems, userAddress);
+                            generateUniqueDocumentId();
                         } else {
                             // Document ID is unique, retrieve item details and save the order
-                            saveOrderWithUniqueDocumentId(documentId, addedItems, userAddress);
+                            saveOrderWithUniqueDocumentId(documentId);
                         }
                     } else {
                         Toast.makeText(OrderReceipt.this, "Failed to check onDelivery collection.", Toast.LENGTH_SHORT).show();
@@ -69,7 +77,7 @@ public class OrderReceipt extends AppCompatActivity {
                 });
     }
 
-    private void saveOrderWithUniqueDocumentId(String documentId, AddedItems addedItems, String userAddress) {
+    private void saveOrderWithUniqueDocumentId(String documentId) {
         Map<String, Object> orderData = new HashMap<>();
         orderData.put("date_ordered", Timestamp.now());
         orderData.put("order_icon", addedItems.getOrderIcon());
@@ -77,8 +85,15 @@ public class OrderReceipt extends AppCompatActivity {
         orderData.put("order_items", addedItems.getCartItems());
         orderData.put("order_status", "ORDERED");
         orderData.put("total_amount", addedItems.getTotalAmount());
-        orderData.put("user_address", userAddress);
+        orderData.put("delivery_address", currentDefaultAddress);
         orderData.put("user_id", addedItems.getUserId());
+        orderData.put("mode_of_payment", modeOfPayment);
+        orderData.put("additional_message", additionalMessage);
+
+        if (modeOfPayment.equals("GCash")){
+            orderData.put("reference_number", "put-the-reference-number-here");
+            orderData.put("uploaded_receipt", "link-to-uploaded-receipt");
+        }
 
         db.collection("pendingOrders")
                 .document(documentId)
